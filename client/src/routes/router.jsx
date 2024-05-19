fbf683f4d734f0a3236fef11f33b7fd61d5d0058
Zihadul Islam <zihadislam2429@gmail.com>
2024-05-19 17:58:00 +0600

    Theme, Pages, Routes Setup Done

---
+++ client/src/routes/router.jsx
@@ -0,0 +1,28 @@
+import {createBrowserRouter} from "react-router-dom";
+import RootLayout from "../RootLayout";
+import Home from "../pages/home/Home";
+import Login from "../pages/auth/login/Login";
+import SignUp from "../pages/auth/signup/SignUp";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+    ],
+  },
+]);
+
+export default router;
