ca98ffe505e33a3d8baecd6202c1c56c50c75dd5
Zihadul Islam <zihadislam2429@gmail.com>
2024-05-22 23:32:10 +0600

    Home Posts Fetched

--- client/src/components/common/Posts.jsx
+++ client/src/components/common/Posts.jsx
@@ -1,25 +1,65 @@
-import {POSTS} from "../../utils/db/dummy";
+/* eslint-disable react/prop-types */
+import {useEffect} from "react";
 import PostSkeleton from "../skeletons/PostSkeleton";
 import Post from "./Post";
+import {useQuery} from "@tanstack/react-query";
 
-const Posts = () => {
-  const isLoading = true;
+const Posts = ({feedType}) => {
+  const getPostEndpoint = () => {
+    switch (feedType) {
+      case "forYou":
+        return "/api/posts/";
+      case "following":
+        return "/api/posts/following";
+      default:
+        return "api/posts/";
+    }
+  };
+
+  const POST_ENDPOINT = getPostEndpoint();
+
+  const {
+    data: posts,
+    isLoading,
+    refetch,
+    isRefetching,
+  } = useQuery({
+    queryKey: ["posts"],
+    queryFn: async () => {
+      try {
+        const res = await fetch(POST_ENDPOINT);
+        const data = await res.json();
+
+        if (!res.ok) {
+          throw new Error(data.message || "Something went wrong");
+        }
+
+        return data.data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  });
+
+  useEffect(() => {
+    refetch();
+  }, [feedType, refetch]);
 
   return (
     <>
-      {isLoading && (
+      {(isLoading || isRefetching) && (
         <div className="flex flex-col justify-center">
           <PostSkeleton />
           <PostSkeleton />
           <PostSkeleton />
         </div>
       )}
-      {!isLoading && POSTS?.length === 0 && (
+      {!isLoading && !isRefetching && posts?.length === 0 && (
         <p className="text-center my-4">No posts in this tab. Switch 👻</p>
       )}
-      {!isLoading && POSTS && (
+      {!isLoading && !isRefetching && posts && (
         <div>
-          {POSTS.map((post) => (
+          {posts.map((post) => (
             <Post key={post._id} post={post} />
           ))}
         </div>
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -31,7 +31,7 @@ const HomePage = () => {
         </div>
 
         <CreatePost />
-        <Posts />
+        <Posts feedType={feedType} />
       </div>
     </>
   );
