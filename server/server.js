3bd58588b6f2b0e79bbb5cfdc9d6ae00f11fae7e
Zihadul Islam <zihadislam2429@gmail.com>
2024-05-25 04:19:42 +0600

    Deploy Error Resolved

--- server/server.js
+++ server/server.js
@@ -31,10 +31,10 @@ app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
+  app.use(express.static(path.join(__dirname, "client/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   });
 }
 
