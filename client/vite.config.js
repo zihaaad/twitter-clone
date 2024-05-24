1c8d526e8c5ceff09e579d7bbdcc7a0d0ccf1fef
Zihadul Islam <zihadislam2429@gmail.com>
2024-05-25 05:49:44 +0600

    Deployed

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://x-api.codeathletic.com",
         changeOrigin: true,
       },
     },
